6d465b50ba1181a37681926347631462b9974fdd
A.Sharifi <alizadasharifi2022@gmail.com>
2025-01-07 19:34:35 -0600

    fix: update font family, spacing, and border-radius to match design

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ function App() {
     <>
       <div className="flex justify-center">Hello world</div>
       <div className="flex justify-center">
-        <div className="p-8 bg-tint-700 mt-3 shadow-shadow-contactCard rounded-lg text-error-light text-display-200 ">
+        <div className="p-8 bg-tint-700 mt-3 shadow-shadow-contactCard rounded-lg font-estedad text-error-light text-display-200 ">
           Hello world
         </div>
       </div>
