92e56f5cfa2ac339534a76d0b479d30d654ab6f1
A.Sharifi <alizadasharifi2022@gmail.com>
2025-01-18 16:49:36 -0600

    feat: add footer section

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,9 +1,11 @@
 import {Outlet} from 'react-router';
+import Footer from './Footer';
 
 function RootLayout() {
   return (
     <main className="space-y-2">
       <Outlet />
+      <Footer />
     </main>
   );
 }
